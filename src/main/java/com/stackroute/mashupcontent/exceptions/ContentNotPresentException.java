f3641db21ee0ed13a2c8153878392fe559124b32
thotakura.anuradha <thotakura.anuradha@stackroute.in>
2019-05-28 19:21:51 +0530

    new class file

---
+++ src/main/java/com/stackroute/mashupcontent/exceptions/ContentNotPresentException.java
@@ -0,0 +1,8 @@
+package com.stackroute.mashupcontent.exceptions;
+
+
+/*Question not present exception*/
+public class ContentNotPresentException extends Exception {
+
+    public ContentNotPresentException(String message) { super(message); }
+}
